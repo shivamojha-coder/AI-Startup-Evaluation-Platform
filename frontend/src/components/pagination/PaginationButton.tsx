8326cb17942e2b5cc28d161485e49337b20af6a5
shivamojha-coder <mrsnewzebral84@gmail.com>
2026-08-03 11:34:15 +0530

    profile

--- frontend/src/components/pagination/PaginationButton.tsx
+++ frontend/src/components/pagination/PaginationButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion, HTMLMotionProps } from "framer-motion";
+import { motion, type HTMLMotionProps } from "framer-motion";
 
 function cn(...classes: (string | undefined | null | false)[]) {
   return classes.filter(Boolean).join(" ");
